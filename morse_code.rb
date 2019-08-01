9f367e09672547602907df186ab36bed54ba75b9
Gabriel Oarcas <40719741+gabrieloarcas@users.noreply.github.com>
2019-08-01 16:26:28 +0100

    Update morse_code.rb

--- morse_code.rb
+++ morse_code.rb
@@ -28,12 +28,13 @@
     puts "exit"
   end
 
+# SOS
+# Create a program which prints the Morse Code alphabet, from A to Z, using . and - (http://en.wikipedia.org/wiki/Morse_code)
+# Convert your program so that it works using a “range” and a “loop” and a method to produce the same output (less code for the same output)
+# Instead of outputting the alphabet, ask the user to input some text and then convert it to Morse code
+# Add functionality to your program to convert input in Morse code back into English
 
- 
-
-
-
-#My lame attempts until I managed to do it!
+#My 'tries' until I managed to do it!
 
   # array_of_values = morse.collect { |k, v| v }
     
