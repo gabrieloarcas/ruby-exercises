cdd6a9383bebb9ab9d5bd1089845ae842f73cde1
Gabriel Oarcas <40719741+gabrieloarcas@users.noreply.github.com>
2019-08-02 18:02:06 +0100

    Update morse_code.rb

--- morse_code.rb
+++ morse_code.rb
@@ -1,41 +1,37 @@
 
- morse = {"A" => ".-", "B" => "-...", "C" => "-.-.", "D" => "-..", "E" => ".",
-  "F" => "..-.", "G" => "--.", "H" => "....", "I" => "..", "J" => ".---", "K" => "-.-", 
-  "L" => ".-..", "M" => "--", "N" => "-.", "O" => "---", "P" => ".--.", "Q" => "--.-", 
-  "R" => ".-.", "S" => "...", "T" => "-", "U" => "..-", "V" => "...-", "W" => ".--", 
-  "X" => "-..-", "Y" => "-.--", "Z" => "--.."} #I forgot to add digits here :)
+letters = "A".."Z"
+letters = letters.to_a
 
 
-  print "What is your word? "
-  answer = gets.chomp.to_s.upcase
-  array = Array.new
-  array_transformed = array.push(answer.split(//)).flatten!  #This .flatten thing REALLLY....
+morse_letters = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+  "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-",
+  ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
+] 
 
-  array_transformed.each do |l|
-    puts morse[l]
+letters_morse = [letters, morse_letters].transpose.to_h
+puts letters_morse
+
+   print "What is your word? "
+   answer = gets.chomp.to_s.upcase
+   array_answer = Array.new
+   array_answer = array_answer.push(answer.split(//)).flatten!  
+   
+   array_answer.each do |li|
+    puts letters_morse[li]
   end
     
-  print "\tYou want me to 
-  convert input in Morse 
-  code back into English? (y/n): "
+    print "You want me to convert input in Morse code back into English? (y/n): "
   
   confirm = gets.chomp.upcase
-  
+
   if confirm == "Y"
     # puts array2.inspect
-    puts array_transformed.join("").capitalize
+    puts array_answer.join("").capitalize
   else
-    puts "exit"
+    puts "Bye!"
   end
 
-# SOS
-# Create a program which prints the Morse Code alphabet, from A to Z, using . and - (http://en.wikipedia.org/wiki/Morse_code)
-# Convert your program so that it works using a “range” and a “loop” and a method to produce the same output (less code for the same output)
-# Instead of outputting the alphabet, ask the user to input some text and then convert it to Morse code
-# Add functionality to your program to convert input in Morse code back into English
-
-#My 'tries' until I managed to do it!
-
+# --------------------------------------------------------
   # array_of_values = morse.collect { |k, v| v }
     
 # def numbers  
@@ -43,6 +39,12 @@
 #     puts "morse[array[0][#{i}]]".delete_prefix('"').delete_suffix('"')
 #   end
 # end
+
+# morse = {"A" => ".-", "B" => "-...", "C" => "-.-.", "D" => "-..", "E" => ".",
+#   "F" => "..-.", "G" => "--.", "H" => "....", "I" => "..", "J" => ".---", "K" => "-.-", 
+#   "L" => ".-..", "M" => "--", "N" => "-.", "O" => "---", "P" => ".--.", "Q" => "--.-", 
+#   "R" => ".-.", "S" => "...", "T" => "-", "U" => "..-", "V" => "...-", "W" => ".--", 
+#   "X" => "-..-", "Y" => "-.--", "Z" => "--.."} 
 
 # puts numbers
   # puts morse[array[0][1]]
@@ -95,15 +97,15 @@
 
 
 
-# # morse_array.each do |i|
-# #   puts morse_array[i]
-# # end
+# morse_array.each do |i|
+#   puts morse_array[i]
+# end
 
 
 
-# # for answer in lettersa
-# #   if answer == A
-# #   puts "Re-doing when x = " + x.to_s
-# #   end
+# for answer in lettersa
+#   if answer == A
+#   puts "Re-doing when x = " + x.to_s
+#   end
 
-# # end
+# end
