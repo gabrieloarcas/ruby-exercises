e56e302f50e0767f0fbd936a8c344d1cf528beb4
Gabriel Oarcas <40719741+gabrieloarcas@users.noreply.github.com>
2019-08-05 20:23:24 +0100

    Update snakeeyes.rb

--- snakeeyes.rb
+++ snakeeyes.rb
@@ -1,6 +1,25 @@
-print "How many dices would you like to roll? "
-answer = gets.chomp.to_i
-answer.times do
-  random = rand(1..6)
-  puts random
+class Die
+  attr_reader :question, :total, :random
+def initialized
+@question
+@total
+@random = random
 end
+    def roll
+    print "How many dices would you like to roll? "
+    answer = gets.chomp.to_i
+    @random = answer.times.map{ Random.rand(1..6) }
+  
+    puts random 
+    end
+  
+   def total_roll
+     total = @random.sum
+     puts "Your total is #{total}"
+    end
+end
+  
+dice = Die.new
+dice.roll
+dice.total_roll
+
